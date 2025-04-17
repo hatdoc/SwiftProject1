3d27950db3ca07e49a7e29b8fd3743b4f624b340
Jed <hatdoc@naver.com>
2025-04-17 17:17:09 +0800

    Initial Commit

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Jed Park on 17/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ L1 DemoTests/L1_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoTests.swift
+//  L1 DemoTests
+//
+//  Created by Jed Park on 17/4/2025.
+//
+
+import Testing
+@testable import L1_Demo
+
+struct L1_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
