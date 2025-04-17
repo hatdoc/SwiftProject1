6acf4c43ab83abaafdcb483708d464a54b724979
Jed <hatdoc@naver.com>
2025-04-17 19:33:55 +0800

    Swift practice 1. Adding images

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                Image("niagarafalls")
+                    .resizable()
+                    .cornerRadius(15)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text("Niagara Falls")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+            }
         }
-        .padding()
     }
 }
 
