d0c81eb4b47018b2aa89b581850adc04cae10b8e
Jed <hatdoc@naver.com>
2025-04-18 13:07:42 +0800

    Lesson 2

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -11,19 +11,53 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Color(.black)
+            Color(.systemMint)
                 .ignoresSafeArea()
-            VStack {
+            VStack(alignment: .leading, spacing: 20.0) {
                 Image("niagarafalls")
                     .resizable()
                     .cornerRadius(15)
                     .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
                     .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                Text("Niagara Falls")
-                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
+                
+                HStack {
+                    Text("Niagara Falls")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        
+                        Text ("Reviews 261")
+                    }
+                    .foregroundStyle(.orange)
+                    .font(.caption)
+                    
+                }
+                Text("Come visit the falls for an experience of a lifetime.")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+                
             }
+            .padding()
+            .background(Rectangle().foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
         }
     }
 }
